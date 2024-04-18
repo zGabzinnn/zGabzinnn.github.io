05ea7d8428252d9d405c03d8f59fca0acb74322d
Gabriel André <159252329+zGabzinnn@users.noreply.github.com>
2024-04-18 14:36:54 -0300

    Add files via upload

---
+++ atitudinal-php-parte-2.md/trabalhinho.php
@@ -0,0 +1,42 @@
+<?php
+$dados = [
+    "produto" => "Camisa DnD Tamanho M",
+    "data_pagamento" => "01/02/2024",
+    "pago" => false
+];
+
+
+
+
+$aluno = [
+    "nome" => "João",
+    "notas" => [
+        "prova1" => 8,
+        "prova2" => 7,
+        "prova3" => 6,
+        "prova4" => 9
+    ]
+];
+
+
+$alunos = [
+    ["nome" => "João", "idade" => 20],
+    ["nome" => "Maria", "idade" => 22],
+    ["nome" => "José", "idade" => 21]
+];
+
+
+$usuarios = [
+    [
+        "nome" => "João",
+        "idade" => 20,
+        "email" => "email@email.com",
+        "senha" => "123456781"
+    ],
+    [
+        "nome" => "Guilherme",
+        "idade" => 17,
+        "email" => "meu.email@email.com",
+        "senha" => "abc12312312"
+    ]
+];
